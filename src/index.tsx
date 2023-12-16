966862879530e0bbca76292e872c84a9c73a9f30
Nihar Rupareliya <nhr199718@gmail.com>
2023-12-16 22:53:08 +1000

    chore:package file updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
+
+export function multiplyTimesTwo(a: number, b: number): Promise<number> {
+  return Promise.resolve(a * b * 2);
+}
